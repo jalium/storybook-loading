2ec33d0a3e197ccad30ae591828c84984eefa2be
jalium <jaliumj@gmail.com>
2019-10-10 21:38:47 -0400

    🧹 clean up

--- src/stories/AnimationControl.stories.js
+++ src/stories/AnimationControl.stories.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
 import AnimationControl from "../AnimationControl.js";
 
 export default {
